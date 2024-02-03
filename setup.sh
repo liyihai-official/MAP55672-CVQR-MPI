c0308d72d7b4b6eb6a95afab2c4abf0a9218594b
MichealLea <pokemonarrive@gmail.com>
2024-02-03 23:48:33 +0000

    Update

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ export C_INCLUDE_PATH=/opt/homebrew/Cellar/open-mpi/5.0.1/include:$C_INCLUDE_PAT
 
 export LIBRARY_PATH=/opt/homebrew/opt/lapack/lib:$LIBRARY_PATH
 export C_INCLUDE_PATH=/opt/homebrew/opt/lapack/include:$C_INCLUDE_PATH
-
+/opt/homebrew/opt/lapack/include:
 
 export LIBRARY_PATH=/opt/homebrew/opt/openblas/lib:$LIBRARY_PATH
 export C_INCLUDE_PATH=/opt/homebrew/opt/openblas/include:$C_INCLUDE_PATH
